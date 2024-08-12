75144b868daa38bd7be43e96337b1c8f78a95f0a
Lisa <frolovaie9@gmail.com>
2024-08-12 18:55:41 +0100

    Implement user messages by importing the messages module from django.contrib.

--- account/views.py
+++ account/views.py
@@ -87,8 +87,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid(): 
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Changes saved successfully.')
-            return redirect('dashboard')
+            messages.success(request, "Your profile updated successfully.")
+        else:
+            messages.error(request, "Error updating your profile.") 
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
